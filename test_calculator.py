27d22865e3c09f3a3c34ff3f7f67a98c1aff98b6
StarJins <kinjn9495@naver.com>
2022-12-30 01:26:20 -0800

    Add failure case

--- test_calculator.py
+++ test_calculator.py
@@ -5,6 +5,9 @@ class TestCalculator(unittest.TestCase):
     def test_mul(self):
         result = calculator.mul(1, 2)
         self.assertEqual(result, 2)
+    def test_mul2(self):
+        result = calculator.mul(1, 3)
+        self.assertEqual(result, 2)
 
 if __name__ == '__main__':
     unittest.main()
